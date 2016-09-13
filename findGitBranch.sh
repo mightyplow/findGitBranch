e7be90b8714a98b6cc02bc8367d4b42098399bfd
Marcel Pflug <mapf@spreadshirt.net>
2016-09-13 20:31:51 +0200

    trying to find exact branch and then try namespace branch

--- findGitBranch.sh
+++ findGitBranch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function findGitBranch {
-  passedBranch=${1:-latest}
+  local passedBranch=${1:-latest}
   local branch_name='develop'
 
-  if [ "$passedBranch" == "latest" ]; then
+  if [[ "$passedBranch" == "latest" ]]; then
     echo "searching branch with last commit..."
 
     local latest_branch=`git for-each-ref --sort=-committerdate --format="%(refname:short)" --count 1`
@@ -14,13 +14,30 @@ function findGitBranch {
     branch_name="${latest_branch/origin\//}"
     echo "use $branch_name as branch name"
   else
-    # find the correct branch for the passed target (from the remote branches)
-    local found_branch=`git branch -r | grep -Em 1 "\b$passedBranch\b"`
+    # find the exact branch (from the remote branches)
+    echo "looking for exact branch $passedBranch"
+    local found_branch=`git branch -r | grep -Em 1 "\b$passedBranch$"`
     
-    if [ -n $found_branch ]; then
+    if [[ -n "$found_branch" ]]; then
+      # remove the origin part in the branch name
       branch_name=${found_branch/origin\//}
-    fi
+    else
+      local namespaceBranch="${passedBranch/\/*/}"
+      echo "exact branch $passedBranch not found. looking for namespace branch $namespaceBranch"
+
+      found_branch=`git branch -r | grep -Em 1 "\b$namespaceBranch"`
+
+      if [[ -n "$found_branch" ]]; then
+        # remove the origin part in the branch name
+        branch_name=${found_branch/origin\//}
+
+        echo "found branch with namespace $namespaceBranch: $found_branch"
+      else
+        echo "no branch with namespace $namespaceBranch found"
+      fi
+    fi  
   fi
 
+  echo "found branch is $branch_name"
   echo "$branch_name"
 }
