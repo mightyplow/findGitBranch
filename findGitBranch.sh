fe8a6efc7e225e178e85f8d57a6aed5d11143187
Marcel Pflug <mapf@spreadshirt.net>
2016-09-13 19:51:22 +0200

    [refactor] simplification

--- findGitBranch.sh
+++ findGitBranch.sh
@@ -1,34 +1,26 @@
-pipe_target=$target_branch
+#!/bin/bash
 
 function findGitBranch {
-  $passedBranch = ${1:-latest}
+  passedBranch=${1:-latest}
   local branch_name='develop'
 
-  if [ "$pipe_target" == "latest" ]; then
+  if [ "$passedBranch" == "latest" ]; then
     echo "searching branch with last commit..."
 
-      latest_branch=`git for-each-ref --sort=-committerdate --format="%(refname:short)" --count 1`
-      echo "branch with last commit is $latest_branch"
-      
-      branch_name=${latest_branch/origin\//}
-      echo "user $branch_name as branch name"
-      
-      #pipe_target=`grep -Eo '^[^/]+' <<< $branch_name`
-      #echo "pipeline target is $pipe_target"
+    local latest_branch=`git for-each-ref --sort=-committerdate --format="%(refname:short)" --count 1`
+    echo "branch with last commit is $latest_branch"
+    
+    # remove the origin part from the branch name since it's a remote branch
+    branch_name="${latest_branch/origin\//}"
+    echo "use $branch_name as branch name"
   else
     # find the correct branch for the passed target (from the remote branches)
-    found_branch=`git branch -r | grep -m 1 $pipe_target`
+    local found_branch=`git branch -r | grep -Em 1 "\b$passedBranch\b"`
     
-    # strip remote namespace
-    branch_name=${found_branch/origin\//}
-    
-    # if branch for target not found, take develop
-    if [ -z $branch_name ]; then
-      echo "Branch for target $pipe_target not found. Using develop instead"
-      branch_name=develop
-      #pipe_target=develop
+    if [ -n $found_branch ]; then
+      branch_name=${found_branch/origin\//}
     fi
   fi
 
-  echo $branch_name
+  echo "$branch_name"
 }
