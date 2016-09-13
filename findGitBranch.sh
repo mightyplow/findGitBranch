7939f942174312fb7c6cb86a51a8ca2387647720
Marcel Pflug <mapf@spreadshirt.net>
2016-09-13 21:00:12 +0200

    expose branch_name variable; renamed local variables

--- findGitBranch.sh
+++ findGitBranch.sh
@@ -2,42 +2,48 @@
 
 function findGitBranch {
   local passedBranch=${1:-latest}
-  local branch_name='develop'
+
+  # branch_name gets exposed to the containing shell
+  branch_name='develop'
 
   if [[ "$passedBranch" == "latest" ]]; then
     echo "searching branch with last commit..."
 
-    local latest_branch=`git for-each-ref --sort=-committerdate --format="%(refname:short)" --count 1`
-    echo "branch with last commit is $latest_branch"
+    local latestBranch=`git for-each-ref --sort=-committerdate --format="%(refname:short)" --count 1`
+    echo "branch with last commit is $latestBranch"
     
     # remove the origin part from the branch name since it's a remote branch
-    branch_name="${latest_branch/origin\//}"
+    branch_name="${latestBranch/origin\//}"
     echo "use $branch_name as branch name"
   else
+    # Note: this only looks in remote branches
+
     # find the exact branch (from the remote branches)
     echo "looking for exact branch $passedBranch"
-    local found_branch=`git branch -r | grep -Em 1 "\b$passedBranch$"`
+    local foundBranch=`git branch -r | grep -Em 1 "\b$passedBranch$"`
     
-    if [[ -n "$found_branch" ]]; then
+    if [[ -n "$foundBranch" ]]; then
       # remove the origin part in the branch name
-      branch_name=${found_branch/origin\//}
+      branch_name=${foundBranch/origin\//}
     else
       local namespaceBranch="${passedBranch/\/*/}"
       echo "exact branch $passedBranch not found. looking for namespace branch $namespaceBranch"
 
-      found_branch=`git branch -r | grep -Em 1 "\b$namespaceBranch"`
+      foundBranch=`git branch -r | grep -Em 1 "\b$namespaceBranch"`
 
-      if [[ -n "$found_branch" ]]; then
+      if [[ -n "$foundBranch" ]]; then
         # remove the origin part in the branch name
-        branch_name=${found_branch/origin\//}
+        branch_name=${foundBranch/origin\//}
 
-        echo "found branch with namespace $namespaceBranch: $found_branch"
+        echo "found branch with namespace $namespaceBranch: $foundBranch"
       else
         echo "no branch with namespace $namespaceBranch found"
       fi
     fi  
   fi
 
+  # remove whitespaces
+  branch_name=${branch_name//[[:blank:]]/}
+
   echo "found branch is $branch_name"
-  echo "$branch_name"
 }
